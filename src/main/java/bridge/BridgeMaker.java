fe38a5e54a5115eb48f5de000b52836410c19ea0
민주 <hinote444@gmail.com>
2023-10-19 16:26:34 +0900

    refactor(bridgemaker): 하드 코딩 값 Enum클래스로 대체

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import bridge.domain.Command;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +32,8 @@ public class BridgeMaker {
 
     private String makeStep(int step) {
         if (step == 0) {
-            return "D";
+            return Command.MOVE_DOWN.getValue();
         }
-        return "U";
+        return Command.MOVE_UP.getValue();
     }
 }
